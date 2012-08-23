2a62a400b687f61a83399be4dce029d0fa7eaddb
Mohamed Magdy <mohamed.magdy@badrit.com>
2012-08-23 23:32:48 +0200

    Fixing sewar names and adding the Spanish translated mosshaf

--- translations_scrappers/spanish.rb
+++ translations_scrappers/spanish.rb
@@ -2,31 +2,31 @@ require 'mechanize'
 
 module SpanishTranslationScrapper
   def self.scrape
-    sewar_names = [ "al-Fatihah", "al-Baqarah", "Al-Imran", "an-Nisa'", "al-Ma'idah", 
-                   "al-An`am", "al-A`raf", "al-Anfal", "at-Taubah", "Yunus", "Hud", 
-                   "Yusuf", "ar-Ra`d", "Ibrahim", "al-Hijr", "an-Nahl", "bani Isra'il", 
-                   "al-Kahf", "Maryam", "Ta Ha", "al-Anbiya'", "al-Hajj", "al-Mu'minun", 
-                   "an-Nur", "al-Furqan", "ash-Shu`ara'", "an-Naml", "al-Qasas", "al-`Ankabut",
-                   "ar-Rum", "Luqman", "as-Sajdah", "al-Ahzab", "Saba'", "al-Fatir", "Ya Sin",
-                   "as-Saffat", "Sad", "az-Zumar", "al-Mu'min", "Ha Mim Sajdah", "ash-Shura", 
-                   "az-Zukhruf", "ad-Dukhan", "al-Jathiyah", "al-Ahqaf", "Muhammad", "al-Fath", 
-                   "al-Hujurat", "Qaf", "adh-Dhariyat", "at-Tur", "an-Najm", "al-Qamar", "ar-Rahman",
-                   "al-Waqi`ah", "al-Hadid", "al-Mujadilah", "al-Hashr", "al-Mumtahanah", "as-Saff",
-                   "al-Jum`ah", "al-Munafiqun", "at-Taghabun", "at-Talaq", "at-Tahrim", "al-Mulk", 
-                   "al-Qalam", "al-Haqqah", "al-Ma`arij", "Nuh", "al-Jinn", "al-Muzammil", "al-Mudathir",
-                   "al-Qiyamah", "ad-Dahr", "al-Mursalat", "an-Naba'", "an-Nazi`at", "`Abasa", "at-Takwir", "al-Infitar",
-                   "at-Tatfif", "al-Inshiqaq", "al-Buruj", "at-Tariq", "al-A`la", "al-Ghashiyah", "al-Fajr", 
-                   "al-Balad", "ash-Shams", "al-Layl", "ad-Duha", "al-Inshirah", "at-Tin", "al-`Alaq", "Qadr", 
-                   "al-Bayyinah", "al-Zilzal", "al-`Adiyat", "al-Qari`ah", "at-Takathur", "al-`Asr", "al-Humazah", 
-                   "al-Fil", "al-Quraysh", "al-Ma`un", "al-Kauthar", "al-Kafirun", "an-Nasr", "al-Lahab", 
-                   "al-Ikhlas", "al-Falaq", "an-Nas" ]
+    sewar_names = [ "Al-Fatihah", "al-Baqarah", "Al-Imran", "An-Nisa'", "Al-Ma'idah", 
+                   "Al-An`am", "Al-A`raf", "Al-Anfal", "At-Taubah", "Yunus", "Hud", 
+                   "Yusuf", "Ar-Ra`d", "Ibrahim", "Al-Hijr", "An-Nahl", "Al-Israa", 
+                   "Al-Kahf", "Maryam", "TaHa", "Al-Anbiya'", "Al-Hajj", "Al-Mu'minun", 
+                   "An-Nur", "Al-Furqan", "Ash-Shu`ara'", "An-Naml", "Al-Qasas", "Al-`Ankabut",
+                   "Ar-Rum", "Luqman", "As-Sajdah", "Al-Ahzab", "Saba'", "Fatir", "Yasin",
+                   "As-Saffat", "Sad", "Az-Zumar", "Ghafer", "Foselat", "Ash-Shura", 
+                   "Az-Zukhruf", "Ad-Dukhan", "Al-Jathiyah", "Al-Ahqaf", "Muhammad", "Al-Fath", 
+                   "Al-Hujurat", "Qaf", "Adh-Dhariyat", "At-Tur", "An-Najm", "Al-Qamar", "Ar-Rahman",
+                   "Al-Waqi`ah", "Al-Hadid", "Al-Mujadilah", "Al-Hashr", "Al-Mumtahanah", "As-Saff",
+                   "Al-Jum`ah", "Al-Munafiqun", "At-Taghabun", "At-Talaq", "At-Tahrim", "Al-Mulk", 
+                   "Al-Qalam", "Al-Haqqah", "Al-Ma`arij", "Nuh", "Al-Jinn", "Al-Muzammil", "Al-Mudathir",
+                   "Al-Qiyamah", "Ad-Dahr", "Al-Mursalat", "An-Naba'", "An-Nazi`at", "`Abasa", "Al-Takwir", "Al-Infitar",
+                   "At-Motafefeen", "Al-Inshiqaq", "Al-Buruj", "At-Tariq", "Al-A`la", "Al-Ghashiyah", "Al-Fajr", 
+                   "Al-Balad", "Ash-Shams", "Al-Layl", "Ad-Duha", "Al-Inshirah", "At-Tin", "Al-`Alaq", "Qadr", 
+                   "Al-Bayyinah", "Az-Zilzal", "Al-`Adiyat", "Al-Qari`ah", "At-Takathur", "Al-`Asr", "Al-Humazah", 
+                   "Al-Fil", "Quraysh", "Al-Ma`un", "Al-Kauthar", "Al-Kafirun", "An-Nasr", "Al-Lahab", 
+                   "Al-Ikhlas", "Al-Falaq", "An-Nas" ]
                          
     mosshaf = File.new('mosshaf.txt', 'w+') 
     agent = Mechanize.new
     sura_no = 1
     verse_no = 1
     
-    while sura_no < 115
+    while sura_no <= sewar_names.length 
       # Action is the language for example sp is the Spanish
       url = "http://www.mosshaf.com/web/ajax_trgma.php?ShowByVerseNo=1&SuraNo=#{sura_no}&VerseNo=#{verse_no}&Action=sp"
       page = agent.get(url).parser
@@ -51,5 +51,7 @@ module SpanishTranslationScrapper
         verse_no = 1
       end
     end
+        
+    mosshaf.close
   end
 end
